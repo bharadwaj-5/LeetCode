7906f611bf6d702a0c95f199862b30d83c46fc41
Bharadwaj A <141353427+bharadwaj-5@users.noreply.github.com>
2024-08-28 23:17:18 +0530

    Time: 42 ms (9.16%) | Memory: 16.3 MB (99.70%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -6,16 +6,12 @@ class Solution:
         while i < n and j < n:
             while j + 1 < n and nums[j] + 1 == nums[j + 1]:
                 j += 1
+            
             if nums[i] == nums[j]:
                 op.append(str(nums[i]))
             else:
                 op.append(f"{nums[i]}->{nums[j]}")
             j += 1
             i = j
+
         return op
-
-
-                
-
-
-        
