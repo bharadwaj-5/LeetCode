02261006028033063504101e91246ab575344fd5
Bharadwaj A <141353427+bharadwaj-5@users.noreply.github.com>
2024-08-31 20:39:04 +0530

    Time: 447 ms (38.95%) | Memory: 17.1 MB (15.66%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,21 +1,22 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        max_w = 0
-        num_zeros = 0
+        max_width = 0
+        zeros = 0
         n = len(nums)
-        l = 0
+        left = 0
 
-        for r in range(n):
-            if nums[r] == 0:
-                num_zeros += 1
+        for right in range(n):
+            if nums[right] == 0:
+                zeros += 1
 
-            while num_zeros > k:
-                if nums[l] == 0:
-                    num_zeros -= 1
-                l += 1
-            w = r - l + 1
-            max_w = max(max_w, w)
-
-        return max_w
+            while zeros > k:
+                if nums[left] == 0:
+                    zeros -= 1
+                left += 1
             
+            w = right - left + 1
+            max_width = max(max_width, w)
+
+        return max_width
+
         
