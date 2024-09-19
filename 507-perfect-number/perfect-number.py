1890989e0fe5cd78b46f9b20236c2bbf203ae48a
Bharadwaj A <141353427+bharadwaj-5@users.noreply.github.com>
2024-09-19 23:02:09 +0530

    Time: 34 ms (85.38%) | Memory: 16.5 MB (80.62%) - LeetSync

---
+++ 507-perfect-number/perfect-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num == 1:
+            return False
+        
+        summ = 1
+
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                summ += i + num//i
+        
+        return summ == num
+
+        
