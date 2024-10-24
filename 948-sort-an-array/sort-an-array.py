2f35a72144e74a3720a02528b9842a09d0f9b373
Bharadwaj A <141353427+bharadwaj-5@users.noreply.github.com>
2024-10-24 10:01:29 +0530

    Time: 668 ms (77.81%) | Memory: 23.8 MB (88.61%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,37 +1,34 @@
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        def mergesort(arr):
-            if len(arr) > 1:
-                mid = len(arr)//2
+    def sortArray(self, arr: List[int]) -> List[int]:
+        if len(arr) <= 1:
+            return arr
+        
+        mid = len(arr) // 2
 
-                left = arr[:mid]
-                right = arr[mid:]
+        left = arr[:mid]
+        right = arr[mid:]
 
-                mergesort(left)
-                mergesort(right)
+        self.sortArray(left)
+        self.sortArray(right)
 
-                i = j = k = 0
+        i = j = k = 0
 
-                while i < len(left) and j < len(right):
-                    if left[i] < right[j]:
-                        arr[k] = left[i]
-                        i += 1
-                    else:
-                        arr[k] = right[j]
-                        j += 1
-                    k += 1
-                
-                while i < len(left):
-                    arr[k] = left[i]
-                    i += 1
-                    k += 1
-                while j < len(right):
-                    arr[k] = right[j]
-                    j += 1
-                    k += 1
-                return arr
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
             else:
-                return arr
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
 
-        op = mergesort(nums)
-        return op
+        return arr
